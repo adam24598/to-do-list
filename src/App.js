bbc21457880dfb7814f36b3b924490aa135ed63e
adam24598 <64758097+adam24598@users.noreply.github.com>
2020-06-17 14:48:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function TodoApp() {
+  const [todo, setTodo] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+
+  const handleChange = e => {
+    setTodo(e.target.value);
+  };
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        text: todo,
+        completed: false
+      }
+    ]);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (todo === "") return;
+    addTodo();
+    setTodo("");
+  };
+
+  const removeTodo = todoId => {
+    const updatedTodos = todos.filter(todo => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
+
+  const toggleTodo = todoId => {
+    const updatedTodos = todos.map(todo => {
+      return todo.id === todoId
+        ? { ...todo, completed: !todo.completed }
+        : todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={onSubmit}>
+        <label htmlFor="todo">Todo text</label>
+        <br />
+        <input
+          id="todo"
+          className="todo-input"
+          onChange={handleChange}
+          value={todo}
+        />
+        <button type="submit" className="add-btn">
+          Add
+        </button>
+      </form>
+      <div>
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              <span
+                className={todo.completed ? "todo-completed" : undefined}
+                onClick={() => toggleTodo(todo.id)}
+              >
+                {todo.text}
+              </span>
+              <span className="delete-btn" onClick={() => removeTodo(todo.id)}>
+                x
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
-
-export default App;
+          }
